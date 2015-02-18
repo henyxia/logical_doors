e0b1c669b56eb4548b58fe657d26c6b2da95c354
henyxia <henyxia@live.fr>
2015-02-18 22:00:53 +0100

    Added lazy probes

---
+++ LazyProbe.java
@@ -0,0 +1,25 @@
+public class LazyProbe extends Sonde
+{
+	private boolean isSet = false;
+	private boolean status = false;
+
+	public LazyProbe(Composant c, String con)
+	{
+		super(c, con);
+	}
+
+	public void reset()
+	{
+		isSet = false;
+	}
+
+	public boolean getEtat()
+	{
+		if(!isSet)
+		{
+			isSet = true;
+			status = super.getEtat();
+		}
+		return status;
+	}
+}
